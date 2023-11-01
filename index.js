c3bafe28cab0c9fc174e8d6122eb82e747978782
Shah Paran Srijan <mspsrijan@gmail.com>
2023-11-01 20:41:20 +0600

    Added Get single products

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -39,6 +39,11 @@ async function run() {
       res.send(brands);
     });
 
+    app.get("/products", async (req, res) => {
+      const brands = await productsCollection.find().toArray();
+      res.send(brands);
+    });
+
     app.get("/brand/:brandId/products", async (req, res) => {
       const brandId = req.params.brandId;
       const products = await productsCollection.find({ brandId }).toArray();
@@ -57,6 +62,13 @@ async function run() {
       const brandId = req.params.brandId;
       const products = await productsCollection.find({ brandId }).toArray();
       res.send(products);
+    });
+
+    app.get("/product/:productId", async (req, res) => {
+      const productId = req.params.productId;
+      const query = { _id: new ObjectId(productId) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
     });
 
     app.post("/brands", async (req, res) => {
